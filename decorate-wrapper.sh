44b653a027a0f14b22eca8ebcf7ec4542ffe69ec
Brian Claywell <bclaywel@fhcrc.org>
2013-08-19 17:03:26 -0700

    Fixed an order of operations bug in decoration

--- decorate-wrapper.sh
+++ decorate-wrapper.sh
@@ -3,8 +3,8 @@
 source $(dirname $0)/util.sh
 source $1
 
-csvjoin -c "specimen" ${GROUP_BY_SPECIMEN} ${METADATA} | \
-    csvcut -c "specimen,${COLUMNS}" > ${DECORATED_GROUP_BY_SPECIMEN}
+csvcut -c "specimen,${COLUMNS}" ${METADATA} | \
+    csvjoin -c "specimen" ${GROUP_BY_SPECIMEN} - > ${DECORATED_GROUP_BY_SPECIMEN}
 
 # drop duplicate columns (thanks, Erick!)
 #csvcut -c $(head -n 1 addresses.csv | sed "s/,/\n/g" | sort |uniq | paste -s -d",")

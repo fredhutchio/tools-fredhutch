879c0171f323940b9cb94ff243787fbf51f4621f
Brian Claywell <bclaywel@fhcrc.org>
2013-09-12 19:03:35 -0700

    Parallelization and report generation

--- cbrother-wrapper.sh
+++ cbrother-wrapper.sh
@@ -10,12 +10,19 @@ if [[ ${ALLOW_ANCIENT} == "TRUE" ]]; then
     FLAGS="--allow-ancient"
 fi
 
-#ls *.cmd | parallel -j 6 --eta $wd'/code/run_cbrother.py {} {.}.phy cbrother.geno {.}.pp'
-
 python ${SCRIPT_DIR}/setup_cbrother.py ${FLAGS} ${ALIGNMENT} ${GENOTYPE_MAP} cbrother
-for CMD in *.cmd; do
-    PREFIX="${CMD%.cmd}"
-    python ${SCRIPT_DIR}/run_cbrother.py ${CMD} ${PREFIX}.phy cbrother.geno ${PREFIX}.pp
-done
 
-pdftk cbrother.*.pdf cat output ${REPORT}
+# TODO: make this more slurmy
+export SHELL="/bin/bash"
+ls *.cmd | parallel -j 12 ${SCRIPT_DIR}'/run_cbrother.py {} {.}.phy cbrother.geno {.}.pp'
+
+rm -f pproc.csv
+for PPROC in *.pp.pproc; do
+    SEQUENCE=$(echo ${PPROC} | awk -F. '{ print $2 }')
+    IE_COP=$(grep 'number_ie_cop_gelman_rubin:' ${PPROC} | cut -d' ' -f2 | perl -p -e 's/\n//')
+    PCP=$(grep 'number_pcp_gelman_rubin:' ${PPROC} | cut -d' ' -f2 | perl -p -e 's/\n//')
+    echo "${SEQUENCE},${IE_COP},${PCP}" >> pproc.csv
+done
+sort < pproc.csv | sed '1isequence,number_ie_cop_gelman_rubin,number_pcp_gelman_rubin' | csvlook | a2ps - -o - | ps2pdfwr - pproc.pdf
+
+pdftk cbrother.*.pdf pproc.pdf cat output ${REPORT}

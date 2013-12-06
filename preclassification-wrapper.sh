11a690129e0eb046039777e78fb77b892e69b863
Brian Claywell <bclaywel@fhcrc.org>
2013-12-06 15:35:59 -0800

    Separate classification and "preclassification" into different tools.

---
+++ preclassification-wrapper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source $(dirname $0)/util.sh
+source $1
+
+PLACED_SEQS=$(extify jplace ${PLACED_SEQS})
+ALIGNED_SEQS=$(extify fasta ${ALIGNED_SEQS})
+
+guppy redup \
+    -m \
+    -d ${DEDUP_INFO} \
+    -o ${REDUPED_SEQS} \
+    ${PLACED_SEQS}
+
+REDUPED_SEQS=$(extify jplace ${REDUPED_SEQS})
+
+rppr prep_db \
+    -c ${REFPKG} \
+    --sqlite ${CLASS_DB}
+
+guppy classify \
+    --pp \
+    -c ${REFPKG} \
+    --sqlite ${CLASS_DB} \
+    --classifier hybrid2 \
+    --nbc-sequences ${ALIGNED_SEQS} \
+    ${REDUPED_SEQS}
+
+multiclass_concat.py ${CLASS_DB}

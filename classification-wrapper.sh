b6c48693ad1482fd43113503f978beafc92fcf7c
Brian Claywell <bclaywel@fhcrc.org>
2014-02-27 13:11:23 -0800

    Update classification wrapper for classif_table.py.

--- classification-wrapper.sh
+++ classification-wrapper.sh
@@ -3,13 +3,13 @@
 source $(dirname $0)/util.sh
 source $1
 
-classif_rect.py \
-    --want-rank ${WANT_RANK} \
+classif_table.py \
     --specimen-map ${SPLIT_MAP} \
+    --rank ${WANT_RANK} \
+    --tallies-wide ${BY_SPECIMEN} \
+    --by-specimen groupBySpecimen.csv \
     ${CLASS_DB} \
-    ${BY_TAXON} \
-    ${BY_SPECIMEN} \
-    groupBySpecimen.csv
+    ${BY_TAXON}
 
 csvjoin -c "specimen" ${LABEL_MAP} groupBySpecimen.csv | \
     csvcut -C 3 > ${GROUP_BY_SPECIMEN}

7649b2f364574d6fe4222377f7e2a6f757b8fd38
Brian Claywell <bclaywel@fhcrc.org>
2014-09-30 15:04:50 -0700

    Add a dependency on yapp-env and update step.

--- preprocessing-wrapper.sh
+++ preprocessing-wrapper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source $(dirname $0)/util.sh
 source $1
 
 deduplicate_sequences.py \
@@ -9,10 +8,13 @@ deduplicate_sequences.py \
     ${INPUT_SEQS} \
     ${DEDUP_SEQS}
 
-refpkg_align.py align \
-    --use-mpi --mpi-arguments "-n ${GALAXY_SLOTS:-4}" \
-    --output-format fasta \
-    --stdout ${ALIGNED_SCORES} \
-    ${REFPKG} \
-    ${DEDUP_SEQS} \
-    ${ALIGNED_SEQS}
+# adapted from yapp/bin/refpkg_align
+ref_sto=$(taxit rp ${REFPKG} aln_sto)
+profile=$(taxit rp ${REFPKG} profile)
+
+sto=$(mktemp -u).sto
+
+cmalign --cpu ${GALAXY_SLOTS:-4} -o "$sto" --sfile "${ALIGNED_SCORES}" --noprob --dnaout "$profile" "${DEDUP_SEQS}" | grep -E '^#'
+
+esl-alimerge --dna --outformat afa "$ref_sto" "$sto" | \
+    seqmagick convert --output-format fasta --dash-gap - "${ALIGNED_SEQS}"
